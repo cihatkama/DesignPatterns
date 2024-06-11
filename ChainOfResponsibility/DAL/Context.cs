cd76bfbb2c4073546749fbfbe77e3610509470a4
cihatkama <cihatkama@gmail.com>
2024-06-11 23:58:19 +0300

    Created code first migration and ViewModel class

---
+++ ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChainOfResponsibility.DAL
+{
+    public class Context : DbContext
+    {
+        // code first ile veri tabanı ayağa kaldırma yöntemi.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=CYK-VICTUS;initial catalog=DesignPattern1;integrated security=true;");
+        }
+
+        public DbSet<CustomerProcessViewModel> CustomerProcesses { get; set; }
+    }
+}
---
+++ ChainOfResponsibility/DAL/CustomerProcess.cs
@@ -0,0 +1,12 @@
+﻿namespace ChainOfResponsibility.DAL
+{
+    public class CustomerProcessViewModel
+    {
+        public int CustomerProcessID { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string EmployeeName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
